dc4f37641f18ca2e8b0877206b63dadf8ed38c33
Dylan Beattoie <dylan@dylanbeattie.net>
2022-05-17 11:16:38 +0300

    First example of hypermedia pagination

--- dotnet/Autobarn.Website/Controllers/api/VehiclesController.cs
+++ dotnet/Autobarn.Website/Controllers/api/VehiclesController.cs
@@ -1,70 +1,92 @@
-﻿using Autobarn.Data;
+﻿using System;
+using Autobarn.Data;
 using Autobarn.Data.Entities;
 using Autobarn.Website.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Autobarn.Website.Controllers.api {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class VehiclesController : ControllerBase {
-		private readonly IAutobarnDatabase db;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase {
+        private readonly IAutobarnDatabase db;
 
-		public VehiclesController(IAutobarnDatabase db) {
-			this.db = db;
-		}
+        public VehiclesController(IAutobarnDatabase db) {
+            this.db = db;
+        }
 
-		// GET: api/vehicles
-		[HttpGet]
-		public IEnumerable<Vehicle> Get() {
-			return db.ListVehicles();
-		}
+        // GET: api/vehicles
+        [HttpGet]
+        [Produces("application/hal+json")]
+        public IActionResult Get(int index = 0, int count = 10) {
+            var items = db.ListVehicles().Skip(index).Take(count);
+            var total = db.CountVehicles();
+            dynamic _links = Paginate("/api/vehicles", index, count, total);
+            var result = new {
+                _links,
+                items
+            };
+            return Ok(result);
+        }
 
-		// GET api/vehicles/ABC123
-		[HttpGet("{id}")]
-		public IActionResult Get(string id) {
-			var vehicle = db.FindVehicle(id);
-			if (vehicle == default) return NotFound();
-			return Ok(vehicle);
-		}
+        private dynamic Paginate(string url, int index, int count, int total) {
+            dynamic links = new ExpandoObject();
+            links.self = new { href = url };
+            links.final = new { href = $"{url}?index={total - (total % count)}&count={count}" };
+            links.first = new { href = $"{url}?index=0&count={count}" };
+            if (index > 0) links.previous = new { href = $"{url}?index={index - count}&count={count}" };
+            if (index + count < total) links.next = new { href = $"{url}?index={index + count}&count={count}" };
+            return links;
+        }
 
-		// POST api/vehicles
-		[HttpPost]
-		public IActionResult Post([FromBody] VehicleDto dto) {
-			var vehicleModel = db.FindModel(dto.ModelCode);
-			var vehicle = new Vehicle {
-				Registration = dto.Registration,
-				Color = dto.Color,
-				Year = dto.Year,
-				VehicleModel = vehicleModel
-			};
-			db.CreateVehicle(vehicle);
-			return Ok(dto);
-		}
 
-		// PUT api/vehicles/ABC123
-		[HttpPut("{id}")]
-		public IActionResult Put(string id, [FromBody] VehicleDto dto) {
-			var vehicleModel = db.FindModel(dto.ModelCode);
-			var vehicle = new Vehicle {
-				Registration = dto.Registration,
-				Color = dto.Color,
-				Year = dto.Year,
-				ModelCode = vehicleModel.Code
-			};
-			db.UpdateVehicle(vehicle);
-			return Ok(dto);
-		}
+        // GET api/vehicles/ABC123
+        [HttpGet("{id}")]
+        public IActionResult Get(string id) {
+            var vehicle = db.FindVehicle(id);
+            if (vehicle == default) return NotFound();
+            return Ok(vehicle);
+        }
 
-		// DELETE api/vehicles/ABC123
-		[HttpDelete("{id}")]
-		public IActionResult Delete(string id) {
-			var vehicle = db.FindVehicle(id);
-			if (vehicle == default) return NotFound();
-			db.DeleteVehicle(vehicle);
-			return NoContent();
-		}
-	}
+        // POST api/vehicles
+        [HttpPost]
+        public IActionResult Post([FromBody] VehicleDto dto) {
+            var vehicleModel = db.FindModel(dto.ModelCode);
+            var vehicle = new Vehicle {
+                Registration = dto.Registration,
+                Color = dto.Color,
+                Year = dto.Year,
+                VehicleModel = vehicleModel
+            };
+            db.CreateVehicle(vehicle);
+            return Ok(dto);
+        }
+
+        // PUT api/vehicles/ABC123
+        [HttpPut("{id}")]
+        public IActionResult Put(string id, [FromBody] VehicleDto dto) {
+            var vehicleModel = db.FindModel(dto.ModelCode);
+            var vehicle = new Vehicle {
+                Registration = dto.Registration,
+                Color = dto.Color,
+                Year = dto.Year,
+                ModelCode = vehicleModel.Code
+            };
+            db.UpdateVehicle(vehicle);
+            return Ok(dto);
+        }
+
+        // DELETE api/vehicles/ABC123
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id) {
+            var vehicle = db.FindVehicle(id);
+            if (vehicle == default) return NotFound();
+            db.DeleteVehicle(vehicle);
+            return NoContent();
+        }
+    }
 }
---
+++ examples/DynamicExamples/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Dynamic;
+
+dynamic d = new ExpandoObject();
+d.a = new ExpandoObject();
+d.a.b = new ExpandoObject();
+d.a.b.c = "hello!";
+d.foo = "bar";
+
+Console.WriteLine(d.a.b.c);
